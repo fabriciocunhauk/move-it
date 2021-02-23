45030d14d0e6f15d4a686a2392485fd1e34ab686
fabricio cunha <fabriciocunhadeveloper@gmail.com>
2021-02-23 03:23:27 +0000

    Created experience bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import ExperienceBar from './components/ExperienceBar';
+import './styles/global.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
+    <div className="container">
+      <ExperienceBar />
     </div>
   );
 }
